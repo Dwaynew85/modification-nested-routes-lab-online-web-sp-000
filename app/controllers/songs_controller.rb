70617fd728fbcf8ad568c99a9dd34ebcbe3304c4
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-21 18:28:54 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,8 +27,10 @@ class SongsController < ApplicationController
   def new
     if params[:artist_id] && !Artist.exists?(params[:artist_id])
       redirect_to artist_path, alert: 'Artist not found.'
-    else
+    elsif params[:artist_id]
       @song = Song.new(artist_id: params[:artist_id])
+    else 
+      @song = Song.new
     end
   end
 
